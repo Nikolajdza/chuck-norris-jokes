78883b65ea6b981174ed9fabe6bbdcde03eeef62
Nikola Buljčik <nikolajdza@gmail.com>
2021-10-25 15:17:42 +0200

    jokes are working ;)

--- script/chuckjokes.js
+++ script/chuckjokes.js
@@ -1,21 +1,26 @@
 export async function randomChuckJoke() {
 	const response = await fetch('http://api.icndb.com/jokes/random');
 	const data = await response.json();
-	console.log(data);
 	return data;
 }
 
 export async function randomChuckJokes(number) {
 	const response = await fetch(`http://api.icndb.com/jokes/random/${number}`);
 	const data = await response.json();
-	console.log(data);
 	return data;
 }
 
 export async function jokeCategories() {
 	const response = await fetch(`http://api.icndb.com/categories`);
 	const data = await response.json();
-	console.log(data);
+	return data;
+}
+
+export async function randomNotExplicitJoke() {
+	const response = await fetch(
+		`http://api.icndb.com/jokes/random?limitTo=[nerdy] `
+	);
+	const data = await response.json();
 	return data;
 }
 
@@ -24,6 +29,13 @@ export async function userChuckJoke(firstName, lastName) {
 		`http://api.icndb.com/jokes/random?firstName=${firstName}&lastName=${lastName}`
 	);
 	const data = await response.json();
-	console.log(data);
 	return data;
+}
+
+export async function displayJoke() {
+	const exclude = document.getElementById('explicitCheck').checked;
+	let joke = exclude ? await randomNotExplicitJoke() : await randomChuckJoke();
+	let output = document.getElementById('output');
+	output = joke.value.joke;
+	return output;
 }
--- script/index.js
+++ script/index.js
@@ -1,3 +1,10 @@
 import * as chuck from './chuckjokes.js';
 
 chuck.randomChuckJoke();
+
+const randomJoke = document.getElementById('get-joke-btn');
+randomJoke.addEventListener('click', async (e) => {
+	e.preventDefault();
+	const output = document.getElementById('output');
+	output.innerHTML = await chuck.displayJoke();
+});
